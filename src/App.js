156b3159b807a90b171376dbcb8fb67e7b47b700
Tanya Bansal <91904607+22tanyabansal@users.noreply.github.com>
2023-02-18 23:16:38 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Header from './Header';
+import FirstSection from "./box1";
+import SecondSection from "./box2";
+import ThirdSection from "./box3";
+import ForthSection from "./box4";
+import FifthSection from "./box5";
+import SixthSection from "./box6";
+import SeventhSection from "./box7";
+import EighthSection from "./box8";
+import FAQSection from "./box9";
+import Footer from "./Footer";
+
+const App = () => {
+    return (
+    <>
+        <Header></Header>
+        <FirstSection></FirstSection>
+        <SecondSection></SecondSection>
+        <ThirdSection></ThirdSection>
+        <ForthSection></ForthSection>
+        <FifthSection></FifthSection>
+        <SixthSection></SixthSection>
+        <SeventhSection></SeventhSection>
+        <EighthSection></EighthSection>
+        <FAQSection></FAQSection>
+        <Footer></Footer>
+    </>
+)
+}
+
+export default App;
